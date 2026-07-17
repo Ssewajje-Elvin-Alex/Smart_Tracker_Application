1999dc3933562bdf00f7dbb16f9952e31a6668fd
Alex jr <elvinalexjrssewajje08@gmail.com>
2026-07-18 02:09:49 +0300

    Added URL render

--- smart_tracker_app/lib/services/api_services.dart
+++ smart_tracker_app/lib/services/api_services.dart
@@ -3,15 +3,19 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String url =
-      //"http://10.52.58.48:8000/api/location/latest/";
-      "http://127.0.0.1:8000/api/location/latest/";
+      "https://smart-tracker-a4mu.onrender.com/api/location/latest/";
+      //"http://10.177.149.48:8000/api/location/latest/";
+      //"http://127.0.0.1:8000/api/location/latest/"; 
 
   static Future<Map<String, dynamic>> getLatestLocation() async {
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
+      print (response.body);
       return jsonDecode(response.body);
     } else {
+      print (response.statusCode);
+      print (response.body);
       throw Exception("Failed to load location");
     }
   }
